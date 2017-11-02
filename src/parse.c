447dc906385f617ccb55c922e6447145e485d107
alyptik <alyptik@protonmail.com>
2017-11-01 22:13:42 -1000

    parse.c: add code to read binary pgp files

--- src/parse.c
+++ src/parse.c
@@ -15,11 +15,77 @@ int munmap(void *addr, size_t length);
 
 size_t read_pgp_bin(char const *restrict filename, pgp_list *restrict list)
 {
-	(void)filename;
+	FILE *file;
+	pgp_packet cur;
+	size_t cnt = 0;
+
+	if (!list)
+		ERR("read_pgp_bin() NULL pgp_list");
+
 	free_pgp_list(list);
 	init_pgp_list(list);
+	if (!(file = fopen(filename, "rb")))
+		ERR("read_pgp_bin() fopen()");
 
-	return 0;
+	if ((cnt += fread(&cur.pheader, 1, sizeof cur.pheader, file)) == 0) {
+		fclose(file);
+		return cnt;
+	}
+
+	if ((cur.pheader & (0x03 << 6)) & F_OLD) {
+		switch (cur.pheader & 0x03) {
+		case L_ONE:
+			if ((cnt += fread(&cur.plen_one, 1, sizeof cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			if (!(cur.pdata = calloc(cur.plen_one, sizeof *cur.pdata))) {
+				fclose(file);
+				ERR("read_pgp() cur.plen_one calloc()");
+			}
+			if ((cnt += fread(&cur.pdata, 1, cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			break;
+
+		case L_TWO:
+			if ((cnt += fread(&cur.plen_one, 1, sizeof cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			if (!(cur.pdata = calloc(cur.plen_one, sizeof *cur.pdata))) {
+				fclose(file);
+				ERR("read_pgp() cur.plen_one calloc()");
+			}
+			if ((cnt += fread(&cur.pdata, 1, cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			break;
+
+		case L_FOUR:
+			if ((cnt += fread(&cur.plen_one, 1, sizeof cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			if (!(cur.pdata = calloc(cur.plen_one, sizeof *cur.pdata))) {
+				fclose(file);
+				ERR("read_pgp() cur.plen_one calloc()");
+			}
+			if ((cnt += fread(&cur.pdata, 1, cur.plen_one, file)) == 0) {
+				fclose(file);
+				ERR("unknown file format");
+			}
+			break;
+
+		case L_OTHER: /* fallthrough */
+		default:;
+		}
+	}
+
+	fclose(file);
+	return cnt;
 }
 
 size_t read_pgp_aa(char const *restrict filename, pgp_list *restrict list)
