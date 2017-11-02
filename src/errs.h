2ddc20642ac435848607bee1bf2e4a661ef3b52a
alyptik <alyptik@protonmail.com>
2017-11-01 16:36:52 -1000

    derpgp.c: remove LICENSE header

--- src/errs.h
+++ src/errs.h
@@ -5,25 +5,10 @@
  *		Santiago Torres <sangy@riseup.net>
  *
  * See LICENSE.md file for copyright and license details.
- *
- * __OR__
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
-
-#ifndef ERRS_H
-#define ERRS_H 1
+#ifndef _ERRS_H
+#define _ERRS_H 1
 
 #include <assert.h>
 #include <errno.h>
