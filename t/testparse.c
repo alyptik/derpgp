c177b38d73ad3866fd41be83d3dbe1ba69bf3c91
Santiago Torres <torresariass@gmail.com>
2017-11-02 21:53:27 -0400

    ENH: add more thorough testing.

--- t/testparse.c
+++ t/testparse.c
@@ -15,12 +15,26 @@ int main(void)
 	char vec_bin[] = "./t/4yyylmao.gpg";
 	pgp_list packets = {0};
 
+	/* by manually inspecting the key, we infer this is the actual data */
+	int expected_pheaders[] = {
+		T_SECKEY,
+		T_UID,
+		T_SIG,
+		T_SECSUBKEY,
+		T_SIG	
+	};
+
 	/* start test block */
-	plan(2);
+	plan(8);
 
 	/* tests */
 	ok(1 == 1, "test ayy lmao");
 	ok(read_pgp_bin(NULL, vec_bin, &packets) > 0, "test binary parsing");
+
+	ok(packets.cnt == 5, "binary parsed the 5 available packets");
+	for (size_t i = 0; i < packets.cnt; i++) 
+		ok((packets.list[i].pheader & expected_pheaders[i]) != 0,
+				"header %u is the correct tag", i);
 
 	/* cleanup */
 	free_pgp_list(&packets);
