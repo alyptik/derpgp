5a71f350e3f51c7651cd527e5dc369c62d21e804
alyptik <alyptik@protonmail.com>
2017-11-05 21:05:29 -1000

    testpacket.c: move comments

--- t/testpacket.c
+++ t/testpacket.c
@@ -33,13 +33,13 @@ int main(void)
 	char vec_bin[] = "./t/4yyylmao.gpg";
 	pgp_list packets = {0};
 
-	/* by manually inspecting the key, we infer this is the actual data */
 	/* start test block */
 	plan(4);
 
 	/* tests */
 	ok(read_pgp_bin(NULL, vec_bin, &packets) > 0, "test binary parsing");
 	ok(packets.cnt == 5, "test finding 5 binary packets");
+	/* by manually inspecting the key, we infer this is the actual data */
 	ok((packets.list[0].pheader & (T_SECKEY << 2)) != 0, "test finding secret key header");
 	ok(parse_pubkey_packet(&packets.list[0]) != 0, "test public key packet parsing");
 
