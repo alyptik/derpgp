93a2941c5c0a3ac2018306aa89d4077982c25e86
alyptik <alyptik@protonmail.com>
2017-11-05 19:16:40 -1000

    parse.h: move `dispatch_table` to header

--- src/parse.c
+++ src/parse.c
@@ -9,34 +9,6 @@
 
 #include "parse.h"
 
-/*
- * static function pointer array
- *
- * TODO XXX: implement handlers
- */
-static int (*const dispatch_table[]) = {
-	/* parse_rsrvd, parse_pkesess, parse_skesess, */
-	0, 0, 0,
-	/* parse_opsig, parse_seckey, parse_pubkey, */
-	0, 0, 0,
-	/* parse_cdata, parse_secsubkey, parse_sedat, */
-	0, 0, 0,
-	/* parse_marker, parse_litdata, parse_trust, */
-	0, 0, 0,
-	/* parse_ui, parse_pubsubkey, */
-	0, 0, 0,
-	/* two placeholder indices */
-	0, 0,
-	/* parse_uattr, parse_seipdata, parse_mdcode, */
-	0, 0, 0,
-	/* ten placeholder indices */
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	/* ten placeholder indices */
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	/* parse_prvt0, parse_prvt1, parse_prvt2, parse_prvt3, */
-	0, 0, 0, 0,
-};
-
 /* extern inline prototypes to prevent linker errors */
 extern inline void free_pgp_list(pgp_list *restrict list_struct);
 extern inline void init_pgp_list(pgp_list *restrict list_struct);
--- src/parse.h
+++ src/parse.h
@@ -12,6 +12,34 @@
 
 #include "defs.h"
 
+/*
+ * static function pointer array
+ *
+ * TODO XXX: implement handlers
+ */
+static int (*const dispatch_table[]) = {
+	/* parse_rsrvd, parse_pkesess, parse_skesess, */
+	0, 0, 0,
+	/* parse_opsig, parse_seckey, parse_pubkey, */
+	0, 0, 0,
+	/* parse_cdata, parse_secsubkey, parse_sedat, */
+	0, 0, 0,
+	/* parse_marker, parse_litdata, parse_trust, */
+	0, 0, 0,
+	/* parse_ui, parse_pubsubkey, */
+	0, 0, 0,
+	/* two placeholder indices */
+	0, 0,
+	/* parse_uattr, parse_seipdata, parse_mdcode, */
+	0, 0, 0,
+	/* ten placeholder indices */
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	/* ten placeholder indices */
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	/* parse_prvt0, parse_prvt1, parse_prvt2, parse_prvt3, */
+	0, 0, 0, 0,
+};
+
 size_t read_pgp_aa(FILE *file_ctx, char const *restrict filename, pgp_list *restrict list);
 
 inline void free_pgp_list(pgp_list *restrict list_struct)
