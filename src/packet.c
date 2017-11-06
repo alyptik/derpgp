b8daebe73f61ed6578874906e7c6a27a1b3af6b5
alyptik <alyptik@protonmail.com>
2017-11-06 00:03:07 -1000

    packet.c: use `mpi_offset` for pointer arithmetic

--- src/packet.c
+++ src/packet.c
@@ -15,18 +15,26 @@ size_t parse_pubkey_packet(pgp_packet *restrict packet)
 	 * FIXME: we only support version 4, so bail if the
 	 * version identifier is any different.
 	 */
-	assert(packet->pdata[0] == 4);
-	packet->pubkey.version = 4;
-	packet->pubkey.timestamp = BETOH32(packet->pdata + 1);
-	packet->pubkey.algorithm = packet->pdata[5];
-	assert(packet->pubkey.algorithm == 1);
-
 	mpi tmp = {0};
-	ptrdiff_t mpi_offset = 6;
+	size_t mpi_offset = 0;
+
+	/* one byte */
+	packet->pubkey.version = packet->pdata[0];
+	assert(packet->pubkey.version == 4);
+	mpi_offset++;
+	/* four bytes */
+	packet->pubkey.timestamp = BETOH32(packet->pdata + mpi_offset);
+	mpi_offset += 4;
+	/* one byte */
+	packet->pubkey.algorithm = packet->pdata[mpi_offset];
+	assert(packet->pubkey.algorithm == 1);
+	mpi_offset++;
 	tmp.length = BETOH16(packet->pdata + mpi_offset);
 	mpi_offset += read_mpi(packet->pdata + mpi_offset, &tmp);
+	/* one mpi struct */
 	packet->pubkey.modulus_n = tmp;
 	mpi_offset += read_mpi(packet->pdata + mpi_offset, &tmp);
+	/* one mpi struct */
 	packet->pubkey.exponent = tmp;
 
 	return mpi_offset;
