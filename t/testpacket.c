c0bff90906fa958ad7b4f121708f8fd039ec228e
alyptik <alyptik@protonmail.com>
2017-11-05 20:24:12 -1000

    config.mk: add `-fno-inline` flag to debug build

--- t/testpacket.c
+++ t/testpacket.c
@@ -12,6 +12,17 @@
 #include "../src/parse.h"
 
 /* extern inline prototypes to prevent linker errors */
+extern inline void xmalloc(void *restrict ptr, size_t sz, char const *msg);
+extern inline void xcalloc(void *restrict ptr, size_t nmemb, size_t sz, char const *msg);
+extern inline void xrealloc(void *restrict ptr, size_t sz, char const *msg);
+extern inline size_t xfread(void *restrict ptr, size_t sz, size_t nmemb, FILE *restrict stream);
+extern inline ptrdiff_t free_argv(char ***restrict argv);
+extern inline void strmv(ptrdiff_t off, char *restrict dest, char const *restrict src);
+extern inline ptrdiff_t free_str_list(str_list *restrict plist);
+extern inline void init_str_list(str_list *restrict list_struct, char *restrict init_str);
+extern inline void append_str(str_list *restrict list_struct, char const *restrict string, size_t pad);
+extern inline void init_pgp_list(pgp_list *restrict list_struct);
+extern inline void add_pgp_list(pgp_list *restrict list_struct, pgp_packet const *restrict packet);
 extern inline void free_pgp_list(pgp_list *restrict list_struct);
 extern inline size_t read_pgp_bin(FILE *file_ctx, char const *restrict filename, pgp_list *restrict list);
 
