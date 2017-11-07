ecb828b75e1f8fca627e54e60d8b25e0439e9acd
alyptik <alyptik@protonmail.com>
2017-11-06 16:02:34 -1000

    testpacket.c: add array definition to unit test

--- t/testpacket.c
+++ t/testpacket.c
@@ -11,6 +11,64 @@
 #include "../src/packet.h"
 #include "../src/parse.h"
 
+/*
+ * static function pointer array
+ *
+ * TODO XXX: implement remaining handlers
+ */
+size_t (*const dispatch_table[64][2])(pgp_packet *restrict) = {
+	/*
+	 * {&parse_rsrvd_packet, &free_rsrvd_packet},
+	 * {&parse_pkesess_packet, &free_pkesess_packet},
+	 * {&parse_skesess_packet, &free_skesess_packet},
+	 */
+	{0}, {0}, {0},
+	/*
+	 * {&parse_opsig_packet, &free_opsig_packet},
+	 * {&parse_seckey_packet, &free_seckey_packet},
+	 * {&parse_pubkey_packet, &free_pubkey_packet},
+	 */
+	{0},
+	{&parse_seckey_packet, &free_seckey_packet},
+	{&parse_pubkey_packet, &free_pubkey_packet},
+	/*
+	 * {&parse_cdata_packet, &free_cdata_packet},
+	 * {&parse_secsubkey_packet, &free_secsubkey_packet},
+	 * {&parse_sedat_packet, &free_sedat_packet},
+	 */
+	{0}, {0}, {0},
+	/*
+	 * {&parse_marker_packet, &free_marker_packet},
+	 * {&parse_litdata_packet, &free_litdata_packet},
+	 * {&parse_trust_packet, &free_trust_packet},
+	 */
+	{0}, {0}, {0},
+	/*
+	 * {&parse_ui_packet, &free_ui_packet},
+	 * {&parse_pubsubkey_packet, &free_pubsubkey_packet},
+	 */
+	{0}, {0},
+	/* two placeholder indices */
+	{0}, {0},
+	/*
+	 * {&parse_uattr_packet, &free_uattr_packet},
+	 * {&parse_seipdata_packet, &free_seipdata_packet},
+	 * {&parse_mdcode_packet, &free_mdcode_packet},
+	 */
+	{0}, {0}, {0},
+	/* ten placeholder indices */
+	{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+	/* ten placeholder indices */
+	{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+	/*
+	 * {&parse_prvt{0}_packet, &free_prvt{0}_pacet},
+	 * {&parse_prvt1_packet, &free_prvt1_packet},
+	 * {&parse_prvt2_packet, &free_prvt2_packet},
+	 * {&parse_prvt3_packet, &free_prvt3_packet},
+	 */
+	{0}, {0}, {0}, {0},
+};
+
 /* extern inline prototypes to prevent linker errors */
 extern inline void xmalloc(void *restrict ptr, size_t sz, char const *msg);
 extern inline void xcalloc(void *restrict ptr, size_t nmemb, size_t sz, char const *msg);
@@ -22,10 +80,10 @@ extern inline ptrdiff_t free_str_list(str_list *restrict plist);
 extern inline void init_str_list(str_list *restrict list_struct, char *restrict init_str);
 extern inline void append_str(str_list *restrict list_struct, char const *restrict string, size_t pad);
 extern inline void init_pgp_list(pgp_list *restrict list_struct);
+extern inline size_t free_pubkey_packet(pgp_packet *restrict packet);
+extern inline size_t free_seckey_packet(pgp_packet *restrict packet);
+extern inline void free_pgp_list(pgp_list *restrict pkts);
 extern inline void add_pgp_list(pgp_list *restrict list_struct, pgp_packet const *restrict packet);
-extern inline void free_pgp_pubkey(pgp_packet *restrict packet);
-extern inline void free_pgp_seckey(pgp_packet *restrict packet);
-extern inline void free_pgp_list(pgp_list *restrict list_struct);
 extern inline size_t read_pgp_bin(FILE *file_ctx, char const *restrict filename, pgp_list *restrict list);
 
 int main(void)
@@ -47,7 +105,7 @@ int main(void)
 	ok(parse_pubkey_packet(&packets.list[3]) > 0, "test successful public key packet parsing");
 
 	/* cleanup */
-	free_pgp_pubkey(&packets.list[3]);
+	free_pubkey_packet(&packets.list[3]);
 	/* test vector doesn't have a public key */
 	free_pgp_list(&packets);
 
