388b94dcdaac9cde9c878d4a0dbae136f7bdbc8f
alyptik <alyptik@protonmail.com>
2017-11-07 19:49:02 -1000

    derpgp.c: implement `-i` flag

--- src/derpgp.c
+++ src/derpgp.c
@@ -11,9 +11,6 @@
 #include "parse.h"
 #include <getopt.h>
 
-/* silence linter */
-int getopt_long(int ___argc, char *const ___argv[], char const *__shortopts, struct option const *__longopts, int *__longind);
-
 /* static variables */
 static struct option const long_opts[] = {
 	{"help", no_argument, 0, 'h'},
@@ -29,9 +26,24 @@ extern char *optarg;
 extern int optind, opterr, optopt;
 extern char **environ;
 
-void parse_opts(int argc, char **argv, char const *optstring)
+/* silence linter */
+int getopt_long(int ___argc, char *const ___argv[], char const *__shortopts, struct option const *__longopts, int *__longind);
+
+/* extern inline prototypes to prevent linker errors */
+extern inline void xmalloc(void *restrict ptr, size_t sz, char const *msg);
+extern inline void xcalloc(void *restrict ptr, size_t nmemb, size_t sz, char const *msg);
+extern inline void xrealloc(void *restrict ptr, size_t sz, char const *msg);
+extern inline size_t xfread(void *restrict ptr, size_t sz, size_t nmemb, FILE *restrict stream);
+extern inline ptrdiff_t free_argv(char ***restrict argv);
+extern inline void strmv(ptrdiff_t off, char *restrict dest, char const *restrict src);
+extern inline ptrdiff_t free_str_list(STR_LIST *restrict plist);
+extern inline void init_str_list(STR_LIST *restrict list_struct, char *restrict init_str);
+extern inline void append_str(STR_LIST *restrict list_struct, char const *restrict string, size_t pad);
+
+PGP_LIST parse_opts(int argc, char **argv, char const *optstring)
 {
 	int opt;
+	PGP_LIST pkts = {0};
 
 	/* don't print an error if option not found */
 	opterr = 1;
@@ -44,7 +56,8 @@ void parse_opts(int argc, char **argv, char const *optstring)
 
 		/* input file flag */
 		case 'i':
-			/* TODO XXX: implement */
+			read_pgp_bin(NULL, optarg, &pkts);
+			parse_pgp_packets(&pkts);
 			break;
 
 		/* output file flag */
@@ -67,12 +80,22 @@ void parse_opts(int argc, char **argv, char const *optstring)
 			exit(0);
 		}
 	}
+
+	return pkts;
 }
 
 int main(int argc, char **argv)
 {
 	char const optstring[] = "hvi:o:";
-	parse_opts(argc, argv, optstring);
+	PGP_LIST pkts = parse_opts(argc, argv, optstring);
+
+
+	for (size_t i = 0; i < pkts.cnt; i++)
+		HPRINT(pkts.list[i].pheader);
+
+	/* cleanup */
+	free_pgp_list(&pkts);
+
 	puts("derp derp derp");
 
 	return 0;
