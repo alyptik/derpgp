96ac1c238d68e3a1100c87ec55cbab623f7ae6e2
alyptik <alyptik@protonmail.com>
2017-11-13 13:06:01 -1000

    testpacket.c: swap test vector packet indices

--- t/testpacket.c
+++ t/testpacket.c
@@ -26,8 +26,8 @@ int main(void)
 	/* by manually inspecting the key, we infer this is the actual data */
 	ok(TAGBITS(pkts.list[0].pheader) == TAG_SECKEY, "test secret key header match");
 	ok(TAGBITS(pkts.list[3].pheader) == TAG_SECSUBKEY, "test secret subkey header match");
-	ok(parse_pubkey_packet(&pkts.list[3]) > 0, "test successful public key packet parsing");
-	ok(parse_seckey_packet(&pkts.list[0]) > 0, "test successful sec key packet parsing");
+	ok(parse_pubkey_packet(&pkts.list[0]) > 0, "test successful public key packet parsing");
+	ok(parse_seckey_packet(&pkts.list[3]) > 0, "test successful sec key packet parsing");
 	lives_ok({free_pgp_list(&pkts);}, "test successful packet list cleanup");
 
 	/* return handled */
